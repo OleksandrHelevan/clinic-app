12890c08cd2d4f48f1c6d03ba9c7388995850672
oleksandr <oleksandrhelevan@gmail.com>
2026-03-26 17:36:52 +0200

    Change PatientServiceUnavailableException

--- doctor-service/src/main/java/com/clinicapp/doctorservice/api/exception/PatientServiceUnavailableException.java
+++ doctor-service/src/main/java/com/clinicapp/doctorservice/api/exception/PatientServiceUnavailableException.java
@@ -1,9 +1,5 @@
 package com.clinicapp.doctorservice.api.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.SERVICE_UNAVAILABLE)
 public class PatientServiceUnavailableException extends RuntimeException {
   public PatientServiceUnavailableException(String message, Throwable cause) {
     super(message, cause);
